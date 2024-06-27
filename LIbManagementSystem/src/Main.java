d03af088a03f9680ee6f31cfd5ad84dcd5e9c940
DumiSibekoGH <108549592+DumiSibekoGH@users.noreply.github.com>
2024-06-28 00:55:07 +0200

    Add files via upload

--- LIbManagementSystem/src/Main.java
+++ LIbManagementSystem/src/Main.java
@@ -1,9 +1,12 @@
 
 import Gui.Interface;
 import Gui.Logging;
+import Lib.Availability_Status;
+import Lib.Book;
 import Lib.Library;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -15,6 +18,7 @@ import javafx.stage.Stage;
  *
  */
 public class Main extends Application{
+	private boolean isLoggedIn = true;
 
 	/**
 	 * @param args
@@ -39,20 +43,45 @@ public class Main extends Application{
 		
 		// Search Library button
 		gui.getNav().getMiSearch().setOnAction(event->{
-			
-			gui.bookListing();
+			if(isLoggedIn == false)
+				gui.replaceUI(log.getLoginUI());
+			else
+				gui.bookListing();
+				
+		});
+		
+		//add book
+		gui.getNav().getMiAddBook().setOnAction(event->{
+			gui.addBook();
+			gui.getLibGUI().getSubmitButton().setOnAction(e->{
+				Book newBook = new Book(gui.getLibGUI().getTitleField().getText(), 
+						gui.getLibGUI().getAuthorField().getText(),
+						gui.getLibGUI().getIsbnField().getText(),
+						gui.getLibGUI().getPublisherField().getText(),
+						gui.getLibGUI().getPublicationDateField().getText(),
+						gui.getLibGUI().getPagesField().getText(),
+						Availability_Status.valueOf(gui.getLibGUI().getAvailabilityField().getValue()), 
+						gui.getLibGUI().getKeywordField().getText());
+				
+				lib.push(newBook);
+			});
 		});
 		
 		
 		// LogIn button
 		gui.getNav().getMiLogin().setOnAction(event->{
-			
-			gui.addUI(log.gp);
+			Logging logging = new Logging();
+	        VBox loginUI = logging.getLoginUI();
+
+	        gui.replaceUI(loginUI);
 		});
 			
 		// Logout button
 		gui.getNav().getMiLogOut().setOnAction(event->{
-			gui.addUI(log.logout());
+			if(isLoggedIn == false)
+				gui.replaceUI(log.getLoginUI());
+			else
+				gui.replaceUI(log.logout());
 		});
 		
 		Scene sc = new Scene(gui.getRoot(), 400,400);
